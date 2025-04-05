17caee8e8749b536655c163784549b598207f51c
sid0402 <sagarwal437@gatech.edu>
2025-04-05 17:43:42 -0400

    ignore large files

---
+++ dataloader.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+
+# PyTorch (modeling)
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+class Mat_Dataset(Dataset):
+  def __init__(self, mats, Subject_IDs):
+    
+    self.samples = []
+    self.labels = []
+    
+    for mat in mats:
+      data = datasets[mat]
+      self.samples.append(np.vstack([data.get(key)[0] for key in Subject_IDs]))
+      self.labels.append(np.hstack([data.get(key)[1] for key in Subject_IDs]))
+      
+    self.samples = np.vstack(self.samples)
+    self.labels = np.hstack(self.labels)
+    
+  def __len__(self):
+    return self.samples.shape[0]
+      
+  def __getitem__(self, idx):
+    return self.samples[idx], self.labels[idx]
